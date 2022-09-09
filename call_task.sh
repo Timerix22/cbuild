ff04da5c198cea82a7948eb882e3cc96c7b5686c
timerix <timerix11@gmail.com>
2022-09-09 22:09:07 +0600

    0080

--- call_task.sh
+++ call_task.sh
@@ -7,7 +7,7 @@ source cbuild/init.sh
 
 #pre_build
 clear_dir $OBJDIR
-clear_dir $OUTDIR
+
 if [ -f "$PRE_BUILD_SCRIPT" ]; then
     printf "${BLUE}executing $PRE_BUILD_SCRIPT"
     source "$PRE_BUILD_SCRIPT"
