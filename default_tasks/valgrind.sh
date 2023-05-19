5e23ef8156a85a981d60152990bde53d6e8eefe9
Timerix22 <timerix11@gmail.com>
2023-05-19 23:42:12 +0600

    force valgrind output on crash

--- default_tasks/valgrind.sh
+++ default_tasks/valgrind.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 cd "$OUTDIR"
-valgrind $VALGRIND_ARGS ./$EXEC_FILE
-cat "valgrind.log"
-myprint "${GREEN}valgrind log saved to ${CYAN}$OUTDIR/exec.log"
+rm -f "valgrind.log"
+set +e
+valgrind --log-file=valgrind.log  $VALGRIND_ARGS ./$EXEC_FILE
+set -e
+[ -f "valgrind.log" ] && cat "valgrind.log" || error "valgrind exited with errors"
+myprint "${GREEN}valgrind log saved to ${CYAN}$OUTDIR/valgrind.log"
 cd ..
